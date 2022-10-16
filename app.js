921ff245d5b707557c1bd200a6d854fce2b0422a
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-10-16 23:47:39 +0000

    added post

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 var app = express();
 
 app.use('/static', express.static("public"));
+app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res){
-    res.send('Hello World');
-})
 
 app.get('/hello', function(req, res){
     res.render('task.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req)
 })
 
 app.listen(3000, function(){
