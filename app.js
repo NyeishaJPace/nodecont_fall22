91f7f471e44f22dc57f00d619416f5f2297d6ebd
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-10-19 00:22:44 +0000

    updated axios get

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var app = express();
+const axios = require('axios');
 
 app.use('/static', express.static("public"));
 app.use(express.urlencoded({ extended: true }))
 app.set("view engine", "ejs");
 
 
-app.get('/', function(req, res){
-    Task.find(function(err, task){
-        console.log(task)
-        if(err){
-            res.json({"Error: ": err})
-        } else {
-            res.render('task.ejs', {taskList: task}); 
-        }
-    })
+axios
+    .get("https://xkcd.com/614/info.0.json")
+    .then(function (response){
+        console.log(response);
+    });
+
+// app.get('/', function(req, res){
+//     Task.find(function(err, task){
+//         console.log(task)
+//         if(err){
+//             res.json({"Error: ": err})
+//         } else {
+//             res.render('task.ejs', {taskList: task}); 
+//         }
+//     })
     
-})
+// })
 
 
 app.listen(3000, function(){
